f159637232d35b7a6db546d9f4b74d91bd0379cf
ycjungSubhuman <ycjung@postech.ac.kr>
2018-05-03 20:51:46 +0900

    Implement some more

--- src/Neural.hs
+++ src/Neural.hs
@@ -8,34 +8,41 @@ module Neural where
 
   -- Fully connected neural network described in ADT
   data Network where
-    -- Start layer : data layer. consists of just one data vector.
+    -- Start layer : data layer. consists of just one data vector. (homogeneous)
     StartLayer :: Vector Double -> Network
     -- Hidden layer : consists of three elements
     -- 1. network subset that comes before this layer
-    -- 2. weight matrix of size ((prev layer output dimension), (curr hidden layer dimension)).
-    -- 3. current forward values
+    -- 2. weight matrix of size ((curr hidden layer dimension), (prev layer output dimension+1)).
+    -- 3. current forward values (homogeneous)
     HiddenLayer :: Network -> Matrix Double -> Vector Double -> Network
     -- End layer : softmax layer. consists of three elements
     -- 1. network subset that comes before this layer
-    -- 2. weight vector of size ((prev layer output dimension)).
-    -- 3. current softmax value
-    EndLayer :: Network -> Vector Double -> Vector Double -> Network
+    -- 2. current softmax value (homogeneous)
+    EndLayer :: Network -> Vector Double -> Network
 
   softmax :: Vector Double -> Vector Double
   softmax v = exp v / (size v) |> [normalizer,normalizer..]
     where
       normalizer = (exp v) <.> 1
 
-  jaSoftmax :: Vector Double -> Matrix Double
-
-  sigmoid x = 1.0 / (1 + exp(-x))
-
-  jaSigmoid x = (sigmoid x) * (1 - sigmoid x)
-
   crossEntropy :: Vector Double -> Vector Double -> Double
   crossEntropy t y = -(sumElements $ (t * (log y)) + ((1 - t) * log (1 - y)))
 
-  jaCrossEntropy :: Vector Double -> Double -> Vector Double
+  sigmoid x = 1.0 / (1 + exp(-x))
+
+  -- Jacobian computations
+
+  jaSoftmax :: Vector Double -> Matrix Double
+  jaSoftmax smValue = fromList $ (\(i,j) ->
+    if i==j then smValue!i * (1 - smValue!i) else -(smValue!i * smValue!j)
+    ) `map` zip [0..(size x)-1] [0..(size x)-1]
+
+  jaSigmoid x = (sigmoid x) * (1 - sigmoid x)
+
+  jaLinear x = x
+
+  jaCrossEntropy :: Vector Double -> Vector Double -> Vector Double
+  jaCrossEntropy t y = fromList $ (\i -> (t!i / y!i) - ((1 - t!i) / (1 - y!i))) `map` [0..(size y)-1]
 
   -- Initialize network with zero weights and zero values
   -- The first argument is dimension of hidden layers. 0th element represents dimension of the last hidden layer
@@ -46,33 +53,32 @@ module Neural where
     EndLayer
       (initInnerNetwork hiddenDims inputDim)
       (zeros $ head hiddenDims)
-      (zeros $ head hiddenDims)
       where
         initInnerNetwork hiddenDims inputDim =
           if 1 == length hiddenDims then
             HiddenLayer
-              (StartLayer (zeros inputDim))
-              (zeromat inputDim (head hiddenDims))
-              (zeros (head hiddenDims))
+              (StartLayer (homo $ zeros inputDim))
+              (zeromat (head hiddenDims) (inputDim+1)) -- col+1 for bias
+              (homo $ zeros (head hiddenDims)) -- append 1 for bias
           else
             HiddenLayer
               (initInnerNetwork (tail hiddenDims) inputDim)
-              (zeromat (head (tail hiddenDims)) (head hiddenDims))
-              (zeros (last hiddenDims))
+              (zeromat (head hiddenDims) (head (tail hiddenDims) + 1)) -- col+1 for bias
+              (homo $ zeros (last hiddenDims)) -- append 1 for bias
 
   -- Get a vector value of the last layer of the network
   lastLayerOf :: Network -> Vector Double
   lastLayerOf network = case network of
     StartLayer d -> d
     HiddenLayer _ _ value -> value
-    EndLayer _ _ value -> value
+    EndLayer _ value -> value
 
   -- Prepare Network with input
   feedData :: Vector Double -> Network -> Network
   feedData input network = case network of
-    StartLayer d -> StartLayer input
+    StartLayer d -> StartLayer $ homo input
     HiddenLayer prevLayer weights value -> HiddenLayer (feedData input prevLayer) weights value
-    EndLayer prevLayer weight value -> EndLayer (feedData input prevLayer) weight value
+    EndLayer prevLayer value -> EndLayer (feedData input prevLayer) value
 
   -- Forward data to update values of Network. Leaves weights of layers unchanged.
   -- Use this for prediction
@@ -80,17 +86,20 @@ module Neural where
   forward network = case network of
     StartLayer d -> StartLayer d
     HiddenLayer prevLayer weights _ ->
-      HiddenLayer prevResult weights (sigmoid (weights #> lastLayerOf prevResult))
+      HiddenLayer prevResult weights (homo (sigmoid (weights #> lastLayerOf prevResult)))
         where prevResult = forward prevLayer
-    EndLayer prevLayer weight _ ->
-      EndLayer prevResult weight (softmax $ weight * lastLayerOf prevResult)
+    EndLayer prevLayer _ ->
+      EndLayer prevResult (softmax $ invhomo lastVector) -- remove the last element since it is meaningless
         where prevResult = forward prevLayer
+              lastVector = lastLayerOf prevResult
 
-  -- Backpropagate errors to update weights of Network. Leaves values of layers unchanged.
-  -- Use this for training
-  backward :: Network -> Network
-  backward network = case network of
-    StartLayer d -> StartLayer d
-    HiddenLayer prevLayer _ value -> HiddenLayer prevResult () value
-      where prevResult = backward prevLayer
-    EndLayer prevLayer weight value ->
+  -- Backpropagate errors to update weights of Network. Leaves values of layers unchanged
+  backward eta gt (EndLayer prevLayer value) =
+    EndLayer (innerBackward $ prevLayer ((jaSoftmax value) #> (jaCrossEntropy value gt))) value
+      where
+        innerBackward network nextJ = case network of
+          StartLayer d -> StartLayer d
+          HiddenLayer prevLayer _ values -> HiddenLayer (innerBackward prevLayer jacobian) neweights values
+            where
+              jacobian = nextJ
+              newweights = 
--- src/Tool.hs
+++ src/Tool.hs
@@ -5,3 +5,11 @@ module Tool where
 
   zeromat n m = (n><m) [0,0..]
 
+  -- append 1 at the end
+  homo :: Vector Double -> Vector Double
+  homo v = vjoin [v, vector [1]]
+
+  -- remove 1 at the end
+  invhomo :: Vector Double -> Vector Double
+  invhomo v = subVector 0 (size v - 1) v
+
